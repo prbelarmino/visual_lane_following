3a667fc27d62e5d6f3d5b5eeb890e4250d270ec4
ika Racer <acdc@ika.rwth-aachen.de>
2023-05-29 21:24:22 +0200

    add classical lane detection

--- scripts/deviation_publisher.py
+++ scripts/deviation_publisher.py
@@ -1,9 +1,14 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import rospy
 from std_msgs.msg import Float64
 from sensor_msgs.msg import Image
 import math
+from lane_detection_classical import ClassicalLaneDetector
+from cv_bridge import CvBridge
+import cv2
+import matplotlib.pyplot as plt
+
 
 class DeviationPublisher:
 
@@ -11,14 +16,30 @@ class DeviationPublisher:
 
         self.deviation_pub = rospy.Publisher("/ika_racer/lane_visual_following/lane_deviation", Float64, queue_size=10)
         self.__realsense_image_sub  = rospy.Subscriber("/ika_racer/perception/realsense/camera/color/image_raw", Image, self.__realsense_image_callback)
+        self.__fisheye_image_sub  = rospy.Subscriber("/ika_racer/perception/fisheye_camera/image_raw", Image, self.__fisheye_image_callback)
         self.steering_angle_factor = 1.0
         self.intial_time = rospy.get_time()
-        self.w = 2*math.pi*0.02
+        self.w = 2*math.pi*0.05
+        self.img_bridge = CvBridge()
+        self.cl_detector = ClassicalLaneDetector()
 
     def __realsense_image_callback(self, msg):
 
         self.deviation_pub.publish(math.sin(self.w*(rospy.get_time()-self.intial_time)))
-    
+        self.cl_detector
+
+    def __fisheye_image_callback(self,msg):
+
+        try: 
+            cv_image = self.img_bridge.imgmsg_to_cv2(msg, "bgr8")
+            #cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
+            detected_lane = self.cl_detector.detect_pipeline(cv_image)
+            #cv2.imshow("Detected Lane", detected_lane)
+            #cv2.waitKey(3)
+
+        except Exception as e:
+
+            print(e)
 
 if __name__=="__main__":
 
--- scripts/lane_follower.py
+++ scripts/lane_follower.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import rospy
 from acvp_msgs.msg import DriveCommandStamped
