e1affdd1b710324de18e4d21851151747e0deadc
ika Racer <acdc@ika.rwth-aachen.de>
2023-08-22 10:36:21 +0200

    classical v_3

--- scripts/lane_detection_classical_v4.py
+++ scripts/lane_detection_classical_v4.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import numpy as np
 import cv2
-from random import randrange
 import math
 
 class ClassicalLaneDetector():
@@ -16,26 +15,26 @@ class ClassicalLaneDetector():
         self.edges = np.array([])
         self.masked_edges = np.array([])
         self.output = np.array([])
-        
+
         #### For HSV thresholding --> get_thresholded_hsv
-        self.lower_threshold =  np.array([0,75,70],dtype = "uint8")
-        self.upper_threshold = np.array([200,175,200],dtype = "uint8")
+        self.lower_threshold = np.array([0, 75, 70], dtype="uint8")
+        self.upper_threshold = np.array([200, 175, 200], dtype="uint8")
 
         #### For canny edge detection --> get_canny_edges
-        self.kernel_size = 15 ## Filter size for gaussian blur
+        self.kernel_size = 15  ## Filter size for gaussian blur
         self.threshlow = 180
         self.threshhigh = 250
 
         #### For region masking --> mask_region
         self.mask_points = [(0, 240), (0, 130), (424, 130), (424, 240)]
-        self.rs_mask_vertices = np.array([self.mask_points ], dtype=np.int32)
+        self.rs_mask_vertices = np.array([self.mask_points], dtype=np.int32)
 
         #### For HoughLines --> get_HoughP
-        self.vote_threshold = 20 # minimum number of votes (intersections in Hough grid cell)
-        self.min_linelength = 10 #minimum number of pixels making up a line
-        self.max_linegap = 15 # maximum gap in pixels between connectable line segments
+        self.vote_threshold = 20  # minimum number of votes (intersections in Hough grid cell)
+        self.min_linelength = 10  # minimum number of pixels making up a line
+        self.max_linegap = 15  # maximum gap in pixels between connectable line segments
         self.rho = 0.75
-        self.theta = np.pi/180
+        self.theta = np.pi / 180
 
         #### For get_lane_slopes
 
@@ -47,7 +46,7 @@ class ClassicalLaneDetector():
         self.rs_image_height = 240
         self.rs_image_width = 424
         self.rs_y_line_limit = 150
-        self.window_size = 10
+        self.window_size = 1
         self.color = (255, 255, 0)
         self.font = cv2.FONT_HERSHEY_SIMPLEX
         self.fontScale = 1
@@ -55,143 +54,186 @@ class ClassicalLaneDetector():
         self.d = 200
         self.left_lines_tr = []
         self.right_lines_tr = []
-        
 
-    def lane_transformation(self,lane):
+    def lane_transformation(self, lane):
 
         if not lane is None and len(lane):
- 
+
             xp = np.array([lane[0], lane[2]])
             yp = np.array([lane[1], lane[3]])
-            xp = 0.5 * self.rs_image_width + 0.5 * self.rs_image_height * (xp - self.rs_image_width / 2) / (yp - self.rs_image_height / 2)
-            yp = self.rs_image_height - self.d*(self.rs_image_height - yp)/(yp-self.rs_image_height/2)
-            lane_tr = [xp[0],yp[0],xp[1],yp[1]]
+            xp = 0.5 * self.rs_image_width + 0.5 * self.rs_image_height * (xp - self.rs_image_width / 2) / (
+                        yp - self.rs_image_height / 2)
+            yp = self.rs_image_height - self.d * (self.rs_image_height - yp) / (yp - self.rs_image_height / 2)
+            lane_tr = [xp[0], yp[0], xp[1], yp[1]]
 
-        else: 
+        else:
 
             lane_tr = []
 
         return lane_tr
-    
-    def get_mov_avg(self,line_list,parameters):
 
-        para_len = len(parameters)
+    def extrap_line(self,x_points,y_points):
+
+        parameters = np.polyfit(x_points, y_points, 1)
+        slope = parameters[0]
+        y_int = parameters[1]
+        ym = 130
+        xm = int((ym - y_int) / slope)
+        yh = self.rs_image_height
+        xh = int((yh - y_int) / slope)
+        angle = math.atan(slope)
+        if slope < 0:
+
+            angle += math.radians(180)
+        return np.array([xh,yh,xm,ym,angle])
+    def merge_lines(self,lines_array):
+
+        merged_line = np.array([])
+        points_len = lines_array.shape[0]
+        if points_len:
+            # points_x = np.concatenate((lines_array[:, 0], lines_array[:, 2]))
+            # points_y = np.concatenate((lines_array[:, 1], lines_array[:, 3]))
+            # group = np.split(lines_array, np.where(np.diff(lines_array[:, 4]) > 0.1)[0] + 1)
+            # lines_array[:, 4].sort()
+            # merged_line = np.array(self.extrap_line(points_x,points_y))
+            merged_line = np.average(lines_array, axis=0)
+        return merged_line
+    def get_mov_avg(self, line_list, lines_array):
+
+        points_len = lines_array.shape[0]
+        if points_len:
+            merged_line = self.merge_lines(lines_array)
+            line_list.append(merged_line)
+
         line_len = len(line_list)
-
-        if para_len:
-
-            line_list.append(np.average(parameters, axis=0))
-
-        if line_len > self.window_size or para_len == 0 and line_len > 0:
-
+        if line_len > self.window_size or points_len == 0 and line_len > 0:
             line_list.pop(0)
-
-        if len(line_list): 
+        if len(line_list):
 
             line = np.average(np.array(line_list), axis=0)
-            a = (line[3] - line[1])/(line[2] - line[0])
-            b = line[1] - a*line[0]
-            y1 = self.rs_image_height
-            x1 = (y1 - b)/a
-            y2 = y1/1.5
-            x2 = (y1 - b)/a
-            angle = math.atan(a)
-            
-            if a < 0:
-
-                angle += math.radians(180)
-            line = np.array([x1,y1,x2,y2,angle])
+            extrap_line = np.array(self.extrap_line((line[0],line[2]),(line[1],line[3])))
 
         else:
+            extrap_line = np.array([])
 
-            line = np.array([])
+        return extrap_line
 
-        return line
-    
-    def get_lanes(self,lines):
+    def set_points_order(self,line):
 
-        left_lines_tr = []
-        right_lines_tr = []
+        if line[4] < 0:
+
+            return(line[0])
+    def get_lanes(self, lines):
+
+        left_lines_tr_arr = []
+        self.left_lines_arr = []
+        right_lines_tr_arr = []
+        self.right_lines_arr = []
+
         if not lines is None:
             for line in lines:
-                
-                x1, y1, x2, y2 = line.reshape(4)
-                if x1 != x2:
-                    parameters = np.polyfit((x1, x2), (y1, y2), 1)
-                    slope = parameters[0]
-                    y_int = parameters[1]
-                    yh = self.rs_image_height
-                    xh = int((yh - y_int)/slope)
-                    ym = int(self.rs_image_height/1.5)
-                    xm = int((ym - y_int)/slope)
 
-                    if  abs(slope) > 0.2:
-                        
-                        if slope < 0 and x1 < 212:
+                line = line[0].astype(float)
+                slope = (line[2]-line[0])/ (line[3]-line[1])
+                tr_line = self.lane_transformation([line[0], line[1], line[2], line[3]])
+                extrap_line = self.extrap_line((tr_line[0],tr_line[2]),(tr_line[1],tr_line[3]))
 
-                            #left.append((xh,yh,xm,ym))
-                            left_lines_tr.append(self.lane_transformation([x1,y1,x2,y2]))
+                #if tr_line[0] != tr_line[2]
+                if abs(extrap_line[4]) > 0.7854:
+                 
+                    if slope <= 0 and line[0] < 180 and extrap_line[0] < 212 and extrap_line[0] * math.sin(extrap_line[4]) > -300:
 
-                        elif slope > 0 and x2 >= 212:
+                        left_lines_tr_arr.append([tr_line[0], tr_line[1], tr_line[2], tr_line[3], extrap_line[4]])
+                        self.left_lines_arr.append([line[0], line[1], line[2], line[3], slope])
 
-                            right_lines_tr.append(self.lane_transformation([x1,y1,x2,y2]))
+                    elif  slope >0 and line[2] > 240 and extrap_line[0] >= 212 and  extrap_line[0]* math.sin(extrap_line[4]) < 550:
 
-        self.left_lane = self.get_mov_avg(self.left_lines_list,left_lines_tr)
-        self.right_lane = self.get_mov_avg(self.right_lines_list,right_lines_tr)
+                        self.right_lines_arr.append([line[0], line[1], line[2], line[3], slope])
+                        right_lines_tr_arr.append([tr_line[0], tr_line[1], tr_line[2], tr_line[3], extrap_line[4]])
 
-    def detection_pipeline(self,image):
+        self.left_lane = self.get_mov_avg(self.left_lines_list, np.array(left_lines_tr_arr))
+        self.right_lane = self.get_mov_avg(self.right_lines_list, np.array(right_lines_tr_arr))
+
+        if self.plot_flag:
+            self.left_line = self.merge_lines(np.array(self.left_lines_arr))
+            if self.left_line.shape[0]:
+                self.left_line = self.extrap_line((self.left_line[0], self.left_line[2]),
+                                                  (self.left_line[1], self.left_line[3]))
+            self.right_line = self.merge_lines(np.array(self.right_lines_arr))
+            if self.right_line.shape[0]:
+                self.right_line = self.extrap_line((self.right_line[0], self.right_line[2]),
+                                                  (self.right_line[1], self.right_line[3]))
+
+    def detection_pipeline(self, image):
 
         self.image = image
         self.hsv_image = cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV)
-        self.filtered_hsv = cv2.inRange(self.hsv_image,self.lower_threshold ,self.upper_threshold)
+        self.filtered_hsv = cv2.inRange(self.hsv_image, self.lower_threshold, self.upper_threshold)
         ### returns the edges dtected from canny method
-        blur = cv2.GaussianBlur(self.filtered_hsv,(self.kernel_size, self.kernel_size),0)
+        blur = cv2.GaussianBlur(self.filtered_hsv, (self.kernel_size, self.kernel_size), 0)
         self.edges = cv2.Canny(blur, self.threshlow, self.threshhigh)
         ### takes canny output as input and returns edges isolated from the region of interest
-        mask = np.zeros_like(self.edges) 
+        mask = np.zeros_like(self.edges)
         cv2.fillPoly(mask, self.rs_mask_vertices, self.ignore_mask_color)
         self.masked_edges = cv2.bitwise_and(self.edges, mask)
-        lines = cv2.HoughLinesP(self.masked_edges, self.rho, self.theta, self.vote_threshold , np.array([]),self.min_linelength , self.max_linegap)
+        lines = cv2.HoughLinesP(self.masked_edges, self.rho, self.theta, self.vote_threshold, np.array([]),
+                                self.min_linelength, self.max_linegap)
         self.get_lanes(lines)
 
         if self.plot_flag:
 
             self.output = np.stack((self.edges,) * 3, axis=-1)
+            cv2.line(self.output, (212,0), (212,240), (255, 255, 255), 2)
+            cv2.line(self.output, (180, 0), (180, 240), (255, 255, 255), 2)
+            cv2.line(self.output, (240, 0), (240, 240), (255, 255, 255), 2)
             cv2.line(self.output, self.mask_points[1], self.mask_points[2], (255, 255, 255), 2)
 
             if not lines is None:
 
                 for line in lines:
-
                     line = line[0]
-                    cv2.line(self.output, (int(line[0]), int(line[1])), (int(line[2]), int(line[3])), (randrange(255), randrange(255), randrange(255)), 2)
-                    
-                    
+                    cv2.line(self.output, (int(line[0]), int(line[1])), (int(line[2]), int(line[3])),
+                             (0, 255, 255), 2)
+
+                for line in self.right_lines_arr:
+                    cv2.line(self.output, (int(line[0]), int(line[1])), (int(line[2]), int(line[3])),
+                             (255, 0, 0), 2)
+                for line in self.left_lines_arr:
+                    cv2.line(self.output, (int(line[0]), int(line[1])), (int(line[2]), int(line[3])),
+                             (0, 0, 255), 2)
+
             if self.left_lane.shape[0]:
 
-                self.left_lines_list
                 left_slope = math.degrees(self.left_lane[4])
-                left_line_avg = self.left_lane.astype(int) 
-                #lines = np.array(self.left_lines_list)
-                #left_line_avg = np.average(lines, axis=0)
-                #left_line_avg = left_line_avg.astype(int) 
+                left_line_avg = self.left_line.astype(int)
+                left_lane_avg = self.left_lane.astype(int)
                 org = (50, 50)
-                org2 = (30,85)
-                self.output = cv2.putText(self.output, "{:2.2f}".format(left_slope), org, self.font, self.fontScale, self.color, self.thickness, cv2.LINE_AA)
-                self.output = cv2.putText(self.output, "{:3.2f}".format(self.left_lane[0]), org2, self.font, self.fontScale, self.color, self.thickness, cv2.LINE_AA)               
-                cv2.line(self.output, (left_line_avg[0], left_line_avg[1]), (left_line_avg[2], left_line_avg[3]), (255,255,0), 8)
+                org2 = (30, 85)
+                self.output = cv2.putText(self.output, "{:2.2f}".format(left_slope), org, self.font, self.fontScale,
+                                          self.color, self.thickness, cv2.LINE_AA)
+                self.output = cv2.putText(self.output, "{:3.2f}".format(self.left_lane[0]), org2, self.font,
+                                          self.fontScale, self.color, self.thickness, cv2.LINE_AA)
+                cv2.line(self.output, (left_lane_avg[0], left_lane_avg[1]), (left_lane_avg[2], left_lane_avg[3]),
+                         (0, 255, 0), 8)
+                cv2.line(self.output, (left_line_avg[0], left_line_avg[1]), (left_line_avg[2], left_line_avg[3]),
+                         (0, 155, 0), 8)
 
             if self.right_lane.shape[0]:
-                #right_slope = (self.right_line_avg[3] - self.right_line_avg[1]) / (self.right_line_avg[2] - self.right_line_avg[0])
-                
-                right_slope = math.degrees(self.right_lane[4])
-                right_line_avg = self.right_lane.astype(int)
 
+                right_slope = math.degrees(self.right_lane[4])
+                right_lane_avg = self.right_lane.astype(int)
+                right_line_avg = self.right_line.astype(int)
+                #print(self.right_line)
                 org = (300, 50)
                 org2 = (300, 85)
-                self.output = cv2.putText(self.output, "{:1.2f}".format(right_slope), org, self.font, self.fontScale, self.color, self.thickness, cv2.LINE_AA)
-                self.output = cv2.putText(self.output, "{:3.2f}".format(self.right_lane[0]), org2, self.font, self.fontScale, self.color, self.thickness, cv2.LINE_AA)
-                cv2.line(self.output, (right_line_avg[0], right_line_avg[1]), (right_line_avg[2], right_line_avg[3]), (255,255,0), 8)
+                self.output = cv2.putText(self.output, "{:1.2f}".format(right_slope), org, self.font, self.fontScale,
+                                          self.color, self.thickness, cv2.LINE_AA)
+                self.output = cv2.putText(self.output, "{:3.2f}".format(self.right_lane[0]), org2, self.font,
+                                          self.fontScale, self.color, self.thickness, cv2.LINE_AA)
+                cv2.line(self.output, (right_lane_avg[0], right_lane_avg[1]), (right_lane_avg[2], right_lane_avg[3]),
+                         (0, 255, 0), 8)
+                cv2.line(self.output, (right_line_avg[0], right_line_avg[1]), (right_line_avg[2], right_line_avg[3]),
+                         (0, 155, 0), 8)
 
 
 
--- scripts/lane_follower.py
+++ scripts/lane_follower.py
@@ -12,19 +12,21 @@ class LaneFollower:
 
         self.left_lane = []
         self.right_lane = []
-        self.max_speed = 0.3
-        self.drive_speed = 0.0
-        self.previous_time = rospy.get_time()
-        self.int_error = 0.0
+        self.initial_time_flag = True
+        self.target_speed = 0.55 #range: 0.0(0.5) to 0.7
+        self.target_steering_angle = 1.0 #range: -1.0 to 1.0
+        self.current_speed = 0.0
+        self.int_error_speed = 0.0
+        self.kp_speed = 0.25
+        self.ki_speed = 0.075
         self.ki_speed = 0.3
-        self.kp_steer = -4 #0.3/120
-        self.kd_steer = 1/(self.max_speed*math.cos(math.radians(50))) #0.91 = cos 25
-        self.kp_speed = 0.9
+        self.int_error_steer = 0.0
+        self.kp_steer = -1.0 #0.3/120
+        self.ki_steer = -2
+        self.kd_steer = 1.25/(self.target_speed*math.cos(math.radians(50))) #0.91 = cos 25
         self.yaw = 0.0
         self.delta_x = 0.0
-        #self.target_speed = 0.7 #range: 0.0 to 0.7 
-        self.target_steering_angle = 1.0 #range: 0.0 to 1.0
-        self.max_steering_angle = 0.85
+        
         self.drive_cmd_pub = rospy.Publisher("/ika_racer/locomotion/drive_command", DriveCommandStamped, queue_size=10)
         self.drive_cmd = DriveCommandStamped()
         self.drive_cmd.drive.driver_code = 1
@@ -33,32 +35,14 @@ class LaneFollower:
         self.drive_cmd.drive.acceleration = 0.69
         self.speed_sub  = rospy.Subscriber("/ika_racer/perception/encoder/speed_filtered", Float64Stamped, self.speed_callback)
         self.__lane_deviation_sub  = rospy.Subscriber("/ika_racer/lane_visual_following/lane_deviation", LaneMsg, self.__lane_deviation_callback)
-        self.steering_angle_factor = 1.0
-        self.x_left_offset = -3
-        self.x_right_offset = 400
-        self.upper_right_threshold =  -15
-        self.lower_right_threshold = 10
-        self.right_threshold_flag = False
-        self.upper_left_threshold = 100
-        self.lower_left_threshold = 90
-        self.left_threshold_flag = False
-        self.image_width = 400
+        self.xr_offset = 400
         self.lane_timeout = rospy.get_time()
         self.vehicle_center_dis = 0.25
         self.pixel_to_meter = 0.3/400
-        self.list_cmd =[]
         
     def speed_callback(self,msg):
 
-        self.drive_speed  = msg.data
-
-    def drive_speed_controller(self):
-
-        error = self.max_speed - self.drive_speed 
-        current_time = rospy.get_time()
-        self.int_error += error*(current_time - self.previous_time)
-        self.previous_time = current_time
-        self.drive_cmd.drive.speed = self.kp_speed*error + self.ki_speed*self.int_error
+        self.current_speed  = msg.data
 
     def __lane_deviation_callback(self, msg):
 
@@ -67,20 +51,25 @@ class LaneFollower:
         if len(self.left_lane) + len(self.left_lane) > 0:
             self.lane_timeout = rospy.get_time()
         self.get_deviation_and_slope()
+       
         self.vehicle_controller()
-        self.drive_speed_controller()
-        #self.print_lanes_par()
         
-    
     def vehicle_controller(self):
 
-        #self.drive_speed = 0.3
-        steer_cmd = self.kp_steer*self.delta_x + self.kd_steer*self.drive_speed*math.cos(self.yaw)
-        print(self.delta_x,math.degrees(self.yaw),self.drive_speed,steer_cmd)
-        #self.max_speed = 0.0
-        steer_cmd = np.clip(steer_cmd,-1,1)
-        #print(self.delta_x,math.degrees(self.yaw),self.kp_steer*self.delta_x,self.kd_steer*0.3*math.cos(self.yaw), steer_cmd)
+        if self.initial_time_flag:
+            self.previous_time = rospy.get_time()
+            self.initial_time_flag = False
 
+        speed_error = self.target_speed - self.current_speed 
+        current_time = rospy.get_time()
+        self.int_error_speed += speed_error*(current_time - self.previous_time)
+        self.int_error_steer += self.delta_x*(current_time - self.previous_time)
+        self.previous_time = current_time
+        speed_cmd = self.ki_speed*self.int_error_speed + self.kp_speed*speed_error 
+        steer_cmd = self.kp_steer*self.delta_x + self.kd_steer* self.current_speed *math.cos(self.yaw) + self.ki_steer * self.int_error_steer
+        print(self.delta_x,math.degrees(self.yaw), self.current_speed ,steer_cmd)
+        steer_cmd = np.clip(steer_cmd,-1,1)
+        self.drive_cmd.drive.speed = speed_cmd
         self.drive_cmd.drive.steering_angle = steer_cmd
         if rospy.get_time() - self.lane_timeout > 0.5:
             print("NO LANE TIMEOUT")
@@ -88,25 +77,6 @@ class LaneFollower:
             self.drive_cmd.drive.steering_angle = 0.0
 
         self.drive_cmd_pub.publish(self.drive_cmd)
-
-    def print_lanes_par(self):
-
-        slp_r = "---"
-        x_r = "---"
-        slp_l = "---"
-        x_l = "---"
-        
-        if len(self.left_lane) == 5:
-
-            slp_l = str(self.left_lane[4])[0:4]
-            x_l = str(self.left_lane[0])[0:3]
-            
-        if len(self.right_lane) == 5:
-
-            slp_r = str(self.right_lane[4])[0:4]
-            x_r = str(self.right_lane[0])[0:3]
-
-        print("Left Slope: " + slp_l + ". Left X: " + x_l + ". Right Slope: " + slp_r + ". Right X: " + x_r)
 
     def get_deviation_and_slope(self):
 
@@ -117,7 +87,7 @@ class LaneFollower:
 
         if len(self.right_lane):
 
-            parameters.append([self.right_lane[0]-self.image_width,self.right_lane[4]])
+            parameters.append([self.right_lane[0]-self.xr_offset,self.right_lane[4]])
 
         if len(parameters):
 
@@ -125,11 +95,9 @@ class LaneFollower:
             parameters_np = np.average(parameters_np, axis=0)
             self.yaw = parameters_np[1]
             self.delta_x = self.pixel_to_meter*parameters_np[0]*math.sin(self.yaw) 
-            #+ self.vehicle_center_dis*math.cos(self.yaw)  
-            #print(self.pixel_to_meter*parameters_np[0]*math.sin(self.yaw),self.vehicle_center_dis*math.cos(self.yaw))
+            
     def stop_vehicle(self):
         
-        self.steering_angle_factor = 0.0
         self.drive_cmd.drive.speed = 0.0
         self.drive_cmd_pub.publish(self.drive_cmd)
 
